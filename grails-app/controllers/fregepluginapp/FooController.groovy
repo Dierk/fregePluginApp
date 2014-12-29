fbf85a33e543fdf3ffa61b8135c6ff70cb1f06c0
Dierk Koenig <dierk.koenig@canoo.com>
2014-12-29 16:02:37 +0100

    using Groovy categories to better integrate with Frege data structures

--- grails-app/controllers/fregepluginapp/FooController.groovy
+++ grails-app/controllers/fregepluginapp/FooController.groovy
@@ -1,9 +1,9 @@
 package fregepluginapp
 
-import frege.runtime.Delayed
 
 import static com.canoo.FregeCaller.perform
 import fregepluginapp.FregeCode
+import fregepluginapp.FregeCode$TFrojo as Frojo
 
 class FooController {
 
@@ -17,16 +17,19 @@ class FooController {
 		// calling Frege code that is modifying domain classes returns an action that must be performed
 		perform(FregeCode.twiceFirstname(foo))
 
-		def frojo = fregepluginapp.FregeCode$TFrojo.mk("Dierk",46,"original")
+		def frojo = Frojo.mk("Dierk",46,"original")
 
-		def result = FregeCode.fillNickName(frojo)
+		use FregeCode, Frojo, {
+			def result = frojo.fillNickName()
 
-		render text: """<pre>
-			5 * 3 is $product
-			and the total of [1,2,3] is $total
-			and the doubled Foo Pojo first name is $foo.firstname
-			and the Frojo data nickname is ${result.nickname(result)}
-"""
+			render text: """<pre>
+				5 * 3 is $product
+				and the total of [1,2,3] is $total
+				and the doubled Foo Pojo first name is $foo.firstname
+				and the Frojo data nickname is ${result.nickname()}
+			"""
+		}
+
 	}
 
 	def ttt() {
